679f0d173d9a5530f0eb10e3305f3b31d8e0221b
PEDRO <161548231+TISTONEGI@users.noreply.github.com>
2024-11-26 17:09:51 -0300

    up-commit

--- bibliotecadigital/bibliotecadigital/src/main/java/com/trabalhodeback/bibliotecadigital/controller/BookController.java
+++ bibliotecadigital/bibliotecadigital/src/main/java/com/trabalhodeback/bibliotecadigital/controller/BookController.java
@@ -1,11 +1,5 @@
 package com.trabalhodeback.bibliotecadigital.controller;
 
-import com.trabalhodeback.bibliotecadigital.dto.BookDto;
-import com.trabalhodeback.bibliotecadigital.model.Book;
-import com.trabalhodeback.bibliotecadigital.repository.BookRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,9 +8,22 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import com.trabalhodeback.bibliotecadigital.dto.BookDto;
+import com.trabalhodeback.bibliotecadigital.model.Book;
+import com.trabalhodeback.bibliotecadigital.repository.BookRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/books")
